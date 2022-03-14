14f2b967bea2d5b2bd313715268d62729fc61ecb
fidellim <lim.fidel@gmail.com>
2022-03-14 20:40:42 +0400

    add key features

--- src/components/feature-card-column.js
+++ src/components/feature-card-column.js
@@ -1,47 +1,53 @@
 /** @jsx jsx */
-import { jsx, Image, Box, Heading, Text } from 'theme-ui';
+import { jsx, Image, Box, Heading, Text } from "theme-ui";
 
 export default function FeatureCardColumn({
-  src,
-  altText = 'default alt text',
-  title,
-  text,
+	src,
+	altText = "default alt text",
+	title,
+	text,
 }) {
-  return (
-    <h1>FeatureCardColumn</h1>
-  );
+	return (
+		<Box sx={styles.card}>
+			<Image src={src} altText={altText} sx={styles.img} />
+			<Box sx={styles.wrapper}>
+				<Heading sx={styles.wrapper.title}>{title}</Heading>
+				<Text sx={styles.wrapper.subTitle}>{text}</Text>
+			</Box>
+		</Box>
+	);
 }
 
 const styles = {
-  card: {
-    display: 'flex',
-    alignItems: ['center', 'flex-start'],
-    flexDirection: 'column',
-    mb: -1,
-    textAlign: ['center', null, 'left'],
-    px: [4, null, 0],
-  },
-  img: {
-    mx: ['auto', null, 0],
-    ml: ['auto', null, '-13px'],
-    mb: -2,
-    width: ['80px', null, null, '90px', null, 'auto'],
-  },
-  wrapper: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    title: {
-      fontSize: 3,
-      color: 'heading_secondary',
-      lineHeight: 1.4,
-      fontWeight: 700,
-      mb: [2, null, 3],
-    },
-    subTitle: {
-      fontSize: 1,
-      fontWeight: 400,
-      lineHeight: '1.9',
-    },
-  },
+	card: {
+		display: "flex",
+		alignItems: ["center", "center"],
+		flexDirection: "column",
+		mb: -1,
+		textAlign: ["center", null, "center"],
+		px: [4, null, 0],
+	},
+	img: {
+		mx: ["auto", null, 0],
+		ml: ["auto", null, "-13px"],
+		mb: -2,
+		width: ["80px", null, null, "90px", null, "auto"],
+	},
+	wrapper: {
+		width: "100%",
+		display: "flex",
+		flexDirection: "column",
+		title: {
+			fontSize: 3,
+			color: "heading_secondary",
+			lineHeight: 1.4,
+			fontWeight: 700,
+			mb: [2, null, 3],
+		},
+		subTitle: {
+			fontSize: 1,
+			fontWeight: 400,
+			lineHeight: "1.9",
+		},
+	},
 };
--- src/components/section-header.js
+++ src/components/section-header.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import { Box, Text, Heading } from 'theme-ui';
+import React from "react";
+import { Box, Text, Heading } from "theme-ui";
 
 export default function SectionHeader({ title, slogan, isWhite }) {
-  return (
-    <h1>SectionHeader</h1>
-  );
+	return (
+		<Box sx={{ variant: "sectionHeader" }}>
+			<Text
+				as="p"
+				sx={{
+					variant: "sectionHeader.subTitle",
+					color: isWhite ? "white" : "primary",
+					opacity: isWhite ? 0.7 : 1,
+				}}
+			>
+				{slogan}
+			</Text>
+			<Heading
+				as="h2"
+				sx={{
+					variant: "sectionHeader.title",
+					color: isWhite ? "white" : "heading",
+				}}
+			>
+				{title}
+			</Heading>
+		</Box>
+	);
 }
